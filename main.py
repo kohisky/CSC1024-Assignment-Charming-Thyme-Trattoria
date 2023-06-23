471fd8dc75feb5b809500c0d002c6572d07e7bc7
Alex Chee <xxchee555xx@gmail.com>
2023-06-23 22:52:09 +0800

    Added Skeleton

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+import datetime
+import os
+
+#                                      -------------    Guidelines   --------------
+# Use camelCase for variables and PascalCase for functions
